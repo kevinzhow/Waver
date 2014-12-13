1ed9e88ff0aad8940c2b4965ef2ebd989caf9530
kevinzhow <kevinchou.c@gmail.com>
2014-12-14 03:44:34 +0800

    Initial Commit

---
+++ Waver/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Waver
+//
+//  Created by kevinzhow on 14/12/14.
+//  Copyright (c) 2014年 Catch Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Waver/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Waver
+//
+//  Created by kevinzhow on 14/12/14.
+//  Copyright (c) 2014年 Catch Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
